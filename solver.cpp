e53d65258aa145be2d6c5dbbfaf3f1557c354e38
Steven Keyes <stevenrkeyes@gmail.com>
2017-10-04 22:26:13 -0700

    added function to load dictionary words from file

--- solver.cpp
+++ solver.cpp
@@ -1,7 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
+std::vector<std::string> load_words(const char *filename)
+{
+    std::ifstream file(filename);
+    std::vector<std::string> words;
+    std::copy(std::istream_iterator<std::string>(file),
+              std::istream_iterator<std::string>(),
+              std::back_inserter(words));
+    return words;
+}
 
 int main()
 {
+    std::vector<std::string> words = load_words("/usr/share/dict/words");
+
     std::cout << "Welcome to the Dirty Boggle Solver\n";
     return 0;
 }
