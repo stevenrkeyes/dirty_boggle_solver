2211f72ad0f0a570a9f983a51e02ac468da3df83
Steven Keyes <stevenrkeyes@gmail.com>
2017-10-06 23:18:11 -0700

    Added hash table for storing words, and added is_word function

--- solver.cpp
+++ solver.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
 #include <vector>
 #include <iterator>
+#include <unordered_set>
 
-std::vector<std::string> load_words(const char *filename)
+#define WIDTH  4
+#define HEIGHT 4
+
+std::unordered_set<std::string> load_words(const char *filename)
 {
     std::ifstream file(filename);
-    std::vector<std::string> words;
+
+    std::unordered_set<std::string> words;
+
     std::copy(std::istream_iterator<std::string>(file),
               std::istream_iterator<std::string>(),
-              std::back_inserter(words));
+              std::inserter(words, words.end()));
     return words;
+}
+
+bool is_word(std::string str)
+{
+    static std::unordered_set<std::string> words = load_words("/usr/share/dict/words");
+    return words.end() != words.find(str);
 }
 
 int main()
 {
-    std::vector<std::string> words = load_words("/usr/share/dict/words");
+    std::string foo = "blooooop";
+
+    // Did some tests: doing a linear list search to check if the word exists takes OTO 3 ms,
+    // but doing a hash table lookup takes OTO 200 ns, which is enough to search every 
+    // possible boggle string.
+
+    is_word(foo);
 
     std::cout << "Welcome to the Dirty Boggle Solver\n";
     return 0;
